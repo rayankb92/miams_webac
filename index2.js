23fae60d66ba93509a9547b080ba7ff15ba77bdc
rayan ferradi <ferradi.r@gmail.com>
2022-01-20 01:15:59 +0100

    maj2

---
+++ index2.js
@@ -0,0 +1,26 @@
+const imag = document.querySelector("#img1");
+const img2 = document.querySelector(".img2");
+const img3 = document.querySelector(".img3");
+const img4 = document.querySelector(".img4");
+
+const ring = (key) => {
+  const audio = new Audio();
+  audio.src = "./mp3/" + key + ".mp3";
+  audio.play();
+};
+
+imag.addEventListener("click", (e) => {
+  ring("tajine");
+});
+
+img2.addEventListener("click", (e) => {
+  ring("4x4");
+});
+
+img3.addEventListener("click", (e) => {
+  ring("pq");
+});
+
+img4.addEventListener("click", (e) => {
+  ring("choix");
+});
